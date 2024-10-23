5a93a2a974b2dabf718be9b0d04b2ba18418a209
Léo <leoclipet26@gmail.com>
2024-10-23 12:38:41 +0200

    Enemy move

--- src/PlanteVsZombie/Enemy.cpp
+++ src/PlanteVsZombie/Enemy.cpp
@@ -1,8 +1,8 @@
 #include "pch.h"
 #include "Enemy.h"
 
-Enemy::Enemy(sf::Vector2f position, Behaviour* enemy_behaviour)
-	: Entity("Enemy", 10, position, sf::Color::Red, enemy_behaviour)
+Enemy::Enemy(sf::Vector2f position, sf::Vector2f direction, float speed, Behaviour* enemy_behaviour)
+	: Entity("Enemy", 10, position, sf::Color::Red, enemy_behaviour), mDirection(direction), mSpeed(speed)
 {
 }
 
@@ -12,4 +12,7 @@ Enemy::~Enemy()
 
 void Enemy::Update()
 {
+	sf::Vector2f position = this->getPosition();
+	position += mDirection * mSpeed;
+	this->setPosition(position);
 }
--- src/PlanteVsZombie/Enemy.h
+++ src/PlanteVsZombie/Enemy.h
@@ -3,8 +3,12 @@
 class Enemy : public Entity
 {
 public:
-	Enemy(sf::Vector2f position, Behaviour* enemy_behaviour);
+	Enemy(sf::Vector2f position, sf::Vector2f direction, float speed, Behaviour* enemy_behaviour);
 	virtual ~Enemy();
 
 	void Update() override;
+
+private:
+	sf::Vector2f mDirection;
+	float mSpeed;
 };
